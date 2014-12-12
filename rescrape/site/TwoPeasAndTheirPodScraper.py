ab2f7541d16b7d78e0b5533edf3211c0cd4e141e
Brandon Shin <brandon.shin@gmail.com>
2014-12-12 14:42:54 -0800

    fixed total time

--- rescrape/site/TwoPeasAndTheirPodScraper.py
+++ rescrape/site/TwoPeasAndTheirPodScraper.py
@@ -12,13 +12,7 @@ class TwoPeas(RecipeScraper):
 	def cook_time(self):
 		return self.soup.find("span",{"class": "cooktime"}).get_text()
 	def total_time(self):
-		a = str(self.prep_time())
-		a = a.split(" ")
-		b = str(self.cook_time())
-		b = b.split(" ")
-		a = int(a[0])
-		b = int(b[0])
-		return str(a + b) + " minutes"
+		return self.soup.find("span", {"class":"duration"}).get_text()
 	def ingredients(self):
 		for s in self.soup.findAll("div", {"class": "ingredient"}):
 			yield s.get_text().encode("ascii", "ignore").strip()
