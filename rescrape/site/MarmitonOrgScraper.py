e88e20f6ccf0da967a62f804c78c9dc812e5b70d
Laurent Meyer <laurent@moid.de>
2015-02-21 20:53:06 +0100

    Added some comments and made to code a bit clearer

--- rescrape/site/MarmitonOrgScraper.py
+++ rescrape/site/MarmitonOrgScraper.py
@@ -3,9 +3,13 @@
 __author__ = 'laurentmeyer'
 
 from AbstractScraper import RecipeScraper
+# re is for few parts which are bad structured on the website
 import re
+
+
 class Marmiton(RecipeScraper):
 
+    # Multi-split from SO
     def tsplit(self, s, sep):
         stack = [s]
         for char in sep:
@@ -22,48 +26,51 @@ class Marmiton(RecipeScraper):
     def url(self):
         return self.url
 
+    # Have to parse the string to get the number of servings
     def num_servings(self):
-        pattern = re.compile("(\d.) (personnes)")
         toGet = self.find("p", {"class", "m_content_recette_ingredients"}).find('span').next
         servings = re.search(re.compile('(\d+)( )(personnes)'), toGet).group(1)
         return servings
 
-    def prep_time(self, addMin = True):
+    # Added a default value of type boolean to be able to make a difference between the function called to return an int
+    # (for the calculation of the total) and one called to be printed
+    def prep_time(self, addMin=True):
         duration = self.find("span", {"class", "preptime"}).next.strip()
-        return str(duration)+" mins" if addMin else int(duration)
+        return str(duration) + " mins" if addMin else int(duration)
 
-    def cook_time(self, addMin = True):
+    def cook_time(self, addMin=True):
         duration = self.find("span", {"class", "cooktime"}).next.strip()
-        return str(duration)+" mins" if addMin else int(duration)
+        return str(duration) + " mins" if addMin else int(duration)
 
     def total_time(self):
-        return str(int(self.prep_time(addMin=False))+int(self.cook_time(addMin=False)))+" mins"
+        return str(int(self.prep_time(addMin=False)) + int(self.cook_time(addMin=False))) + " mins"
 
+    # Removed the header not to have a repetition in the output
     def ingredients(self):
-        textToExtract = self.find("p", {"class", "m_content_recette_ingredients"}).text
-        textToExtract = self.tsplit(textToExtract, ("\r","\n"))
+        textToExtract = self.tsplit(self.find("p", {"class", "m_content_recette_ingredients"}).text, ("\r", "\n"))
         for text in textToExtract:
-            if u'Ingrédient'in text or text.strip=='':
+            if u'Ingrédient' in text or text.strip == '':
                 continue
             text = text.strip()
-            text= text.split('-')
+            text = text.split('-')
             for ingredient in text:
-                if ingredient.strip()!='':
+                if ingredient.strip() != '':
                     yield ingredient
 
+    # Not properly implemented on the website; so I had to remove manually useless parts
     def directions(self):
-        alltext = self.find("div", {"class", "m_content_recette_todo"}).text
-        alltext = re.sub(r'\([^)]*\)', '', alltext)
-        alltext = self.tsplit(alltext, ("\r","\n",".","!"))
+        # Removed the parenthesis to be have things clearer
+        alltext = self.tsplit(re.sub(r'\([^)]*\)', '', self.find("div", {"class", "m_content_recette_todo"}).text),
+                              ("\r", "\n", ".", "!"))
         for instruction in alltext:
             if u'Préparation' in instruction:
                 continue
             if u'Remarque' in instruction:
                 break
-            if instruction.strip()!='':
-                instruction = instruction.strip()+".".replace("\d+\/ ","")
-                instruction = re.sub(r'\d\/ ','', instruction)
+            if instruction.strip() != '':
+                instruction = instruction.strip() + ".".replace("\d+\/ ", "")
+                instruction = re.sub(r'\d\/ ', '', instruction)
                 yield instruction
 
     def note(self):
-        return self.findAll("img", {"class":"on"}).count()
+        return self.findAll("img", {"class": "on"}).count()
